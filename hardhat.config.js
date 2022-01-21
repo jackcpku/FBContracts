617486f7a821e48f9039490ab3bd530f88fefdc4
declan.chen <declan.chen@funplus.com>
2022-01-21 10:39:49 +0800

    uncomment accounts task

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,13 +21,13 @@ if (process.env.ACCOUNT_PRIVATE_KEY) {
 
 // // This is a sample Hardhat task. To learn how to create your own go to
 // // https://hardhat.org/guides/create-task.html
-// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-//   const accounts = await hre.ethers.getSigners();
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 //////////////////////////////////////////////////////
 
