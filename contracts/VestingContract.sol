eb107053f4ddd7d2ec825c40b97ca233fa5d2e6a
Jack Cheng <jackcpku@gmail.com>
2022-02-17 17:40:07 +0800

    Remove TODO

--- contracts/VestingContract.sol
+++ contracts/VestingContract.sol
@@ -4,11 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-// TODO
-// 1. proportion -> amount
-// 2. function add(beneficiary, amount)
-// 3. 
-
 contract VestingContract {
     using SafeERC20 for IERC20;
 
