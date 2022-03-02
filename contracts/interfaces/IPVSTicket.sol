57fc7b383b844c81df0ee4800767e8adf7f6162a
Francis <710892027dongyi@gmail.com>
2022-03-02 22:21:35 +0800

    update comments for IPVSTicket

--- contracts/interfaces/IPVSTicket.sol
+++ contracts/interfaces/IPVSTicket.sol
@@ -6,28 +6,26 @@ pragma solidity ^0.8.0;
  */
 interface IPVSTicket {
     /**
-     * @dev Burn `amount` tokens from the _ticketOwner's account to `address(0)`.
+     * @dev Burn `amount` tickets from the _ticketOwner's account to `address(0)`.
      */
     function burn(address _ticketOwner, uint256 _amount) external;
 
      /**
-     * @dev Mint `amount` tokens from the `address(0)` to  _ticketOwner's account.
+     * @dev Mint `amount` tickets from the `address(0)` to  _ticketOwner's account.
      */
     function mint(address _ticketOwner, uint256 _amount) external;
 
      /**
-     * @dev Emitted when `value` tokens are moved from one account (`owner`) to
-     * another (`burner`).
+     * @dev Emitted when `amount` tickets from (`owner`) were burned by (`burner`).
      *
      * Note that `value` may be zero.
      */
-    event TicketBurned(address indexed owner, address indexed burner, uint256 value);
+    event TicketBurned(address indexed owner, address indexed burner, uint256 amount);
 
      /**
-     * @dev Emitted when `value` tokens are moved from one account (`minter`) to
-     * another (`owner`).
+     * @dev Emitted when `amount` tickets were minted by (`minter`) to (`owner`).
      *
      * Note that `value` may be zero.
      */
-    event TicketMinted(address indexed minter, address indexed owner, uint256 value);
+    event TicketMinted(address indexed minter, address indexed owner, uint256 amount);
 }
