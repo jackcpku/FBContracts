d6bf9da465b666f49528d4b8807f3e621a42ba79
Francis <710892027dongyi@gmail.com>
2022-03-09 22:26:37 +0800

    add another version of dividend

---
+++ contracts/AutoDividend.sol
@@ -0,0 +1,160 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+/**
+ * The platform will mint NFT in multiple periods, and addresses holding NFT can get a certain percentage of platform NFT Market revenue dividends.
+ * This Contract is designed for helping the platform to calculate and distribute dividends for each nft in each period.
+ * During a period time, the pvs held by this contract will be evenly distributed to all NFTs that have been minted at the .
+ *
+ * 1. At the beginning of each new period, the dividend pool of the previous period is calculated and locked according to the real-time pvs balance.
+ * 2. The accumulated dividend amount of a certain nft to the current period  = the accumulated dividend amount from the period when it was minted + the dividend amount of current period.
+ */
+
+contract AutoDividend is Ownable {
+    using SafeERC20 for IERC20;
+
+    // The token used to pay dividends
+    address public pvsAddress;
+
+    // PPN address
+    address public tokenAddress;
+
+    // period of divide
+    uint256 public period;
+
+    // Total amount of PVS that has been claimed
+    uint256 public totalClaimed;
+
+    // All pvs(dividend) that have been priced in
+    uint256 public totalPool;
+
+    // Dividends per nft per period
+    uint256[] public dividends;
+
+    // Dividend that has been claimed by this nft's owner
+    mapping(uint256 => uint256) public hasClaimed;
+
+    // startSecond of dividend
+    uint256 public startSecond;
+
+    // each period begins second from the startSecond
+    uint256[] public periodSecond;
+
+    // fixed amount of release in each period
+    uint256 public constant NFT_AMOUNT_RELASED_PER_PERIOD = 2_000;
+
+    event UpdatePeriod(
+        address indexed operator,
+        uint256 newPeriod,
+        uint256 totalAmount
+    );
+
+    event Claim(address indexed receiver, uint256 tokenId, uint256 amount);
+
+    // constructor, init period = 1 
+    constructor(
+        address _pvsAddress,
+        address _tokenAddress,
+        uint256 _start,
+        uint256[] memory _periods
+    ) {
+        pvsAddress = _pvsAddress;
+        tokenAddress = _tokenAddress;
+        startSecond = _start;
+        periodSecond = _periods;
+
+        dividends.push(0);
+        period = 1;
+    }
+
+    /**
+     * Deduce and lock previous period's dividend of each nft
+     * Update totalAmount of nft & maxTokenId in the new period
+     * Update period
+     */
+    function updatePeriod(uint256 _newPeriod) external {
+        require(
+            _newPeriod == period + 1,
+            "Dividend: the new period must be exactly one period after the present"
+        );
+        require(
+            block.timestamp >= startSecond + periodSecond[_newPeriod],
+            "Dividend: the next period has not yet begun"
+        );
+
+        uint256 pool = IERC20(pvsAddress).balanceOf(address(this)) + totalClaimed - totalPool;
+        dividends.push(pool / (NFT_AMOUNT_RELASED_PER_PERIOD * period));
+        totalPool += pool;
+
+        period = _newPeriod;
+
+        emit UpdatePeriod(
+            msg.sender,
+            period,
+            (NFT_AMOUNT_RELASED_PER_PERIOD * period)
+        );
+    }
+
+    /**
+     * Get one nft's minted period
+     */
+    function getPeriod(uint256 _tokenId) internal view returns (uint256) {
+        require(_tokenId > 0 && _tokenId <= (NFT_AMOUNT_RELASED_PER_PERIOD * period), "Dividend: tokenId exceeded limit");
+        return ceilDiv(_tokenId, NFT_AMOUNT_RELASED_PER_PERIOD);
+    }
+
+    /**
+     * Get the total dividend of a nft from its minted period
+     * The accumulated dividend amount of a certain nft to the current period
+     * 	= the accumulated dividend amount from the period when it was minted + the dividend amount of current period.
+     */
+    function totalDividend(uint256 _tokenId) public view returns (uint256) {
+        // get the nft's minted period
+        uint256 beginPeriod = getPeriod(_tokenId);
+        // previous dividends
+        uint256 previous;
+        for (uint256 i = beginPeriod; i < period; i++) {
+            previous += dividends[i];
+        }
+        // current dividend
+        uint256 current = (IERC20(pvsAddress).balanceOf(address(this)) + totalClaimed - totalPool) / (NFT_AMOUNT_RELASED_PER_PERIOD * period);
+        return previous + current;
+    }
+
+    // claim dividend for nft with _tokenId
+    function claim(uint256 _tokenId) public {
+        require(
+            IERC721(tokenAddress).ownerOf(_tokenId) == msg.sender,
+            "Dividend: Can't claim dividend because you are not the owner of the nft"
+        );
+
+        uint256 amount = totalDividend(_tokenId) - hasClaimed[_tokenId];
+        IERC20(pvsAddress).safeTransfer(msg.sender, amount);
+        hasClaimed[_tokenId] += amount;
+        totalClaimed += amount;
+
+        emit Claim(msg.sender, _tokenId, amount);
+    }
+
+    // claim batch
+    function claimBatch(uint256[] calldata _tokenIds) external {
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            claim(_tokenIds[i]);
+        }
+    }
+
+    // remain dividends = the total dividends attributable to this nft - the dividends already been claimed
+    function remainDividend(uint256 _tokenId) public view returns (uint256) {
+        return totalDividend(_tokenId) - hasClaimed[_tokenId];
+    }
+
+    // ceilDiv 
+    function ceilDiv(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b + (a % b == 0 ? 0 : 1);
+    }
+}
---
+++ test/test-auto-dividend-contract.js
@@ -0,0 +1,152 @@
+const { expect } = require("chai");
+const hre = require("hardhat");
+const { deployMajorToken, deployNFTGatewayAndNFTFactory, deployAutoDividend } = require("../lib/deploy");
+
+describe("Test NFT Dividend..........", function () {
+  let pvs, dd, gateway, factory; 
+ 
+  const p1pvs = BigInt(1000000);
+  const p2pvs = BigInt(400000);
+  const p3pvs = BigInt(60000);
+
+  const p1Amt = BigInt(2000);
+  const p2Amt = BigInt(2000);
+  const p3Amt = BigInt(2000);
+
+  const u2nftId = 222;
+
+  let nftContractAddress;
+
+  const startTime = 1700000000;
+  const oneHour = 60 * 60;
+  const oneDay = 24 * oneHour;
+  const sevenDays = 7 * oneDay;
+  const periods = [0, 0, oneHour, oneDay, sevenDays];
+
+  beforeEach("contracts deployed.", async function () {
+    await hre.network.provider.send("hardhat_reset");
+
+    [owner, gatewayAdmin, u1, u2, u3, u4] = await hre.ethers.getSigners();
+    pvs = await deployMajorToken(owner.address);
+
+    //deploy nft factory
+    ({ gateway, factory } = await deployNFTGatewayAndNFTFactory(gatewayAdmin));
+    // Similate the call to obtain the return value.
+    nftContractAddress = await factory
+      .connect(u2)
+      .callStatic.deployBaseERC721("U2-contract", "U2T");
+
+    dd = await deployAutoDividend(pvs.address, nftContractAddress, startTime, periods);
+
+    //set start time for our blockchian
+    await hre.network.provider.send("evm_setNextBlockTimestamp", [startTime]);
+  });
+
+  it("Test Period 1 :", async function() {
+    expect (await dd.totalDividend(1)).to.equal(0);
+    await expect(dd.totalDividend(2001)).to.be.revertedWith("Dividend: tokenId exceeded limit");
+    //add p1pvs to pool[1]
+    await pvs.transfer(dd.address, p1pvs);
+    expect (await dd.totalDividend(1)).to.equal(p1pvs / p1Amt);
+  });
+
+  it("Test Period 2 :", async function() {
+    // add p1pvs to pool[1]
+    await pvs.transfer(dd.address, p1pvs);
+
+    const block = await hre.ethers.provider.getBlock("latest");
+    console.log(block.timestamp);
+    await expect(dd.updatePeriod(2)).to.be.revertedWith("Dividend: the next period has not yet begun");
+
+    // Speed up the clock to the second period 
+    await hre.network.provider.send("evm_setNextBlockTimestamp", [
+        startTime + periods[2],
+    ]);
+    // period 2 begin
+    await dd.updatePeriod(2);
+    await expect(dd.updatePeriod(1)).to.be.revertedWith("Dividend: the new period must be exactly one period after the present");
+    //add p2pvs to pool[2]
+    await pvs.transfer(dd.address, p2pvs);
+    expect (await dd.totalDividend(1))
+      .to.equal(p1pvs / p1Amt + (p2pvs) / (p1Amt + p2Amt));
+
+    expect (await dd.totalDividend(2001))
+      .to.equal(((p2pvs) / (p1Amt + p2Amt)));
+  });
+
+  it("Test Period 3 :", async function() {
+    //add p1pvs to pool[1]
+    await pvs.transfer(dd.address, p1pvs);
+
+    // Speed up the clock to the second period 
+    await hre.network.provider.send("evm_setNextBlockTimestamp", [
+        startTime + periods[2],
+    ]);
+    //period 2 begin
+    await dd.updatePeriod(2);
+    //add p2pvs to pool[2]
+    await pvs.transfer(dd.address, p2pvs);
+
+    // Speed up the clock to the second period 
+    await hre.network.provider.send("evm_setNextBlockTimestamp", [
+        startTime + periods[3],
+    ]);
+    //period 3 begin
+    await dd.updatePeriod(3);
+    await pvs.transfer(dd.address, p3pvs);
+
+    expect (await dd.totalDividend(1))
+      .to.equal(p1pvs / p1Amt + (p2pvs / (p1Amt + p2Amt)) + (p3pvs / (p1Amt + p2Amt + p3Amt)));
+    
+    expect (await dd.totalDividend(2002))
+      .to.equal(p2pvs / (p1Amt + p2Amt) + (p3pvs / (p1Amt + p2Amt + p3Amt)));
+    
+    expect (await dd.totalDividend(5000))
+      .to.equal(p3pvs / (p1Amt + p2Amt + p3Amt));
+  });
+
+  it("Test claim", async function() {
+    //add p1pvs to pool[1]
+    await pvs.transfer(dd.address, p1pvs);
+    // Speed up the clock to the second period 
+    await hre.network.provider.send("evm_setNextBlockTimestamp", [
+        startTime + periods[2],
+    ]);
+    //period 2 begin
+    await dd.updatePeriod(2);
+    //add p2pvs to pool[2]
+    await pvs.transfer(dd.address, p2pvs);
+    // Speed up the clock to the second period 
+    await hre.network.provider.send("evm_setNextBlockTimestamp", [
+        startTime + periods[3],
+    ]);
+    //period 3 begin
+    await dd.updatePeriod(3);
+    await pvs.transfer(dd.address, p3pvs);
+
+    // Let u2 deploy the contract.
+    let u2Contract = await hre.ethers.getContractAt(
+      "ERC721Base",
+      nftContractAddress
+    );
+    // Let u2 deploy the contract.
+    await factory.connect(u2).deployBaseERC721("U2-contract", "U2T");
+    await gateway.connect(u2).ERC721_mint(nftContractAddress, u2.address, u2nftId);
+    expect(await u2Contract.ownerOf(u2nftId)).to.equal(u2.address);
+
+    //claim
+    await expect(dd.connect(u1).claim(u2nftId)).to.be.revertedWith("Dividend: Can't claim dividend because you are not the owner of the nft");
+    expect(await dd.connect(u2).claim(u2nftId)).to.emit(dd, "Claim").withArgs(u2.address, u2nftId, await dd.totalDividend(u2nftId));
+    expect (await dd.connect(u2).remainDividend(u2nftId)).to.equal(0);
+
+    //claim Batch
+    const u2nftId1 = 100, u2nftId2 = 2200, u2nftId3 = 4500;
+    await gateway.connect(u2).ERC721_mint(nftContractAddress, u2.address, u2nftId1);
+    await gateway.connect(u2).ERC721_mint(nftContractAddress, u2.address, u2nftId2);
+    await gateway.connect(u2).ERC721_mint(nftContractAddress, u2.address, u2nftId3);
+    const claimBatchEvent = await dd.connect(u2).claimBatch([u2nftId1, u2nftId2, u2nftId3]);
+    expect(claimBatchEvent).to.emit(dd, "Claim").withArgs(u2.address, u2nftId1, await dd.totalDividend(u2nftId1));
+    expect(claimBatchEvent).to.emit(dd, "Claim").withArgs(u2.address, u2nftId2, await dd.totalDividend(u2nftId2));
+    expect(claimBatchEvent).to.emit(dd, "Claim").withArgs(u2.address, u2nftId3, await dd.totalDividend(u2nftId3));
+  });
+});
