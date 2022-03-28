8944c22de7db72afb669024725eeb81976def3be
Jack Cheng <jackcpku@gmail.com>
2022-03-28 21:12:13 +0800

    Rewrite SimpleLootBoxRegistry

---
+++ contracts/SimpleLootBoxRegistry.sol
@@ -0,0 +1,216 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./nft/interfaces/INFTGateway.sol";
+
+contract SimpleLootBoxRegistry is Ownable {
+    address nftGateway;
+
+    /**
+     * erc1155TokenAddress => erc1155TokenId => tree
+     */
+    mapping(address => mapping(uint256 => bool[])) trees;
+
+    /**
+     * Number of items in the given lootbox
+     *
+     * erc1155TokenAddress => erc1155TokenId => size
+     */
+    mapping(address => mapping(uint256 => uint256)) erc721Sizes;
+
+    /**
+     * erc1155TokenAddress => erc1155TokenId => erc721LowerBounds
+     */
+    mapping(address => mapping(uint256 => uint256)) erc721LowerBounds;
+
+    /**
+     * erc1155TokenAddress => erc1155TokenId => erc721Address
+     */
+    mapping(address => mapping(uint256 => address)) erc721TokenAddresses;
+
+    event GetRandomIndex(address indexed tokenAddress, uint256 indexed tokenId);
+
+    event Full(address indexed sender);
+
+    constructor(address _nftGateway) {
+        nftGateway = _nftGateway;
+    }
+
+    // boxNFT & contentNFT
+    function configLootBox(
+        address _contentAddress,
+        uint256 _erc721LowerBound,
+        uint256 _erc721UpperBound,
+        address _erc1155TokenAddress,
+        uint256 _erc1155TokenId
+    ) external onlyOwner {
+        require(
+            erc721Sizes[_erc1155TokenAddress][_erc1155TokenId] == 0,
+            "SimpleLootBoxRegistry: erc1155 already initialized"
+        );
+
+        trees[_erc1155TokenAddress][_erc1155TokenId] = new bool[](
+            2 * (_erc721UpperBound - _erc721LowerBound + 1) - 1
+        );
+        erc721LowerBounds[_erc1155TokenAddress][
+            _erc1155TokenId
+        ] = _erc721LowerBound;
+        erc721Sizes[_erc1155TokenAddress][
+            _erc1155TokenId
+        ] = (_erc721UpperBound - _erc721LowerBound + 1);
+        erc721TokenAddresses[_erc1155TokenAddress][
+            _erc1155TokenId
+        ] = _contentAddress;
+    }
+
+    /**
+     * This function
+     */
+    function unwrapLootBox(
+        address _erc1155TokenAddress,
+        uint256 _erc1155TokenId
+    ) external returns (uint256 randomTokenId) {
+        randomTokenId = _getRandom(_erc1155TokenAddress, _erc1155TokenId);
+        if (randomTokenId == 0) return 0;
+
+        INFTGateway(nftGateway).ERC1155_burn(
+            _erc1155TokenAddress,
+            msg.sender,
+            _erc1155TokenId,
+            1
+        );
+
+        // address(this) must be added to NFTGateway's whitelist
+        INFTGateway(nftGateway).ERC721_mint(
+            erc721TokenAddresses[_erc1155TokenAddress][_erc1155TokenId],
+            msg.sender,
+            randomTokenId
+        );
+    }
+
+    /**
+     * Get a random number in range of [lowerBound, upperBound].
+     * One number can only be picked up once. If all 2000 numbers are
+     * taken, return 0.
+     */
+    function _getRandom(address _erc1155TokenAddress, uint256 _erc1155TokenId)
+        internal
+        returns (uint256 result)
+    {
+        if (trees[_erc1155TokenAddress][_erc1155TokenId][0]) {
+            emit Full(msg.sender);
+            return 0;
+        }
+
+        uint256 rand = uint256(
+            keccak256(abi.encodePacked(msg.sender, block.timestamp))
+        );
+
+        uint256 currentNode = 0;
+        while (
+            _inTree(
+                _erc1155TokenAddress,
+                _erc1155TokenId,
+                _getLeftChild(currentNode)
+            )
+        ) {
+            // When currentNode's left child is in tree, node is an internal node.
+
+            // If one and only one of children nodes is empty, go the empty path.
+            if (
+                _queryNode(
+                    _erc1155TokenAddress,
+                    _erc1155TokenId,
+                    _getLeftChild(currentNode)
+                )
+            ) {
+                currentNode = _getRightChild(currentNode);
+                continue;
+            } else if (
+                _queryNode(
+                    _erc1155TokenAddress,
+                    _erc1155TokenId,
+                    _getRightChild(currentNode)
+                )
+            ) {
+                currentNode = _getLeftChild(currentNode);
+                continue;
+            }
+
+            // If both children nodes are empty, roll a dice
+            if (rand % 2 == 0) {
+                currentNode = _getLeftChild(currentNode);
+            } else {
+                currentNode = _getRightChild(currentNode);
+            }
+            rand = rand / 2;
+        }
+
+        // At this point, currentNode is an empty leaf node.
+        assert(!_queryNode(_erc1155TokenAddress, _erc1155TokenId, currentNode));
+
+        _setNode(_erc1155TokenAddress, _erc1155TokenId, currentNode);
+        result =
+            currentNode +
+            1 -
+            erc721Sizes[_erc1155TokenAddress][_erc1155TokenId] +
+            erc721LowerBounds[_erc1155TokenAddress][_erc1155TokenId];
+
+        emit GetRandomIndex(_erc1155TokenAddress, result);
+
+        // Traverse back to root
+        while (
+            _queryNode(
+                _erc1155TokenAddress,
+                _erc1155TokenId,
+                _getBrother(currentNode)
+            ) && currentNode != 0
+        ) {
+            currentNode = _getParent(currentNode);
+            _setNode(_erc1155TokenAddress, _erc1155TokenId, currentNode);
+        }
+    }
+
+    function _setNode(
+        address _erc1155TokenAddress,
+        uint256 _erc1155TokenId,
+        uint256 x
+    ) internal {
+        trees[_erc1155TokenAddress][_erc1155TokenId][x] = true;
+    }
+
+    function _queryNode(
+        address _erc1155TokenAddress,
+        uint256 _erc1155TokenId,
+        uint256 x
+    ) internal view returns (bool) {
+        return trees[_erc1155TokenAddress][_erc1155TokenId][x];
+    }
+
+    function _inTree(
+        address _erc1155TokenAddress,
+        uint256 _erc1155TokenId,
+        uint256 _x
+    ) internal view returns (bool) {
+        return _x < 2 * erc721Sizes[_erc1155TokenAddress][_erc1155TokenId] - 1;
+    }
+
+    function _getBrother(uint256 _x) internal pure returns (uint256) {
+        if (_x == 0) return 0;
+        else if (_x % 2 == 0) return _x - 1;
+        else return _x + 1;
+    }
+
+    function _getParent(uint256 _x) internal pure returns (uint256) {
+        return (_x - 1) / 2;
+    }
+
+    function _getLeftChild(uint256 _x) internal pure returns (uint256) {
+        return 2 * _x + 1;
+    }
+
+    function _getRightChild(uint256 _x) internal pure returns (uint256) {
+        return 2 * _x + 2;
+    }
+}
--- test/test-lootbox.js
+++ test/test-lootbox.js
@@ -1,27 +1,80 @@
 const { expect } = require("chai");
 const { BigNumber } = require("ethers");
 const hre = require("hardhat");
-const { deployLootBox } = require("../lib/deploy.js");
+const {
+  deployLootBox,
+  deployNFTGatewayAndNFTFactory,
+} = require("../lib/deploy.js");
+const {
+  calculateCreate2AddressBasicERC721,
+  calculateCreate2AddressBasicERC1155,
+} = require("../lib/create2.js");
+
+// TODO
 
 describe("Test LootBox Contract", function () {
-  let lootBox;
+  let lootBox, factory, gateway, u2Contract;
 
-  beforeEach("Deploy contract", async function () {
+  beforeEach("Deploy contracts", async function () {
     // Reset test environment.
     await hre.network.provider.send("hardhat_reset");
 
+    [
+      owner,
+      gatewayAdmin,
+      newGatewayAdmin,
+      u2,
+      u3,
+      u4,
+      gatewayManager3,
+      u5,
+      u6,
+    ] = await hre.ethers.getSigners();
+
+    ({ gateway, factory } = await deployNFTGatewayAndNFTFactory(gatewayAdmin));
+
     lootBox = await deployLootBox();
+
+    // Deploy ERC721 contract
+
+    const from = factory.address;
+    const deployeeName = "BasicERC721";
+    const tokenName = "U2-contract";
+    const tokenSymbol = "U2T";
+    const baseURI = "baseURI";
+    const salt = 233;
+    const u2ContractAddress = await calculateCreate2AddressBasicERC721(
+      from,
+      deployeeName,
+      tokenName,
+      tokenSymbol,
+      baseURI,
+      gateway.address,
+      salt
+    );
+
+    // Let u2 deploy the contract.
+    await factory
+      .connect(u2)
+      .deployBasicERC721(tokenName, tokenSymbol, baseURI, salt);
+    u2Contract = await hre.ethers.getContractAt(
+      "BasicERC721",
+      u2ContractAddress
+    );
+    expect(await u2Contract.gateway()).to.equal(gateway.address);
   });
 
-  it.only("should get some random number between 1 and 2000", async function () {
+  it.skip("should pass test", async function () {
     this.timeout(100000);
+
+    await lootBox.initialize(u2Contract.address, 1, 2000);
 
     let randoms = [];
     for (let i = 1; i <= 2000; i++) {
       if (i % 100 == 0) {
         console.log(`Done ${i} iterations`);
       }
-      const tx = await lootBox.getRandom();
+      const tx = await lootBox.mintLootBox(u2Contract.address, 0);
       const rc = await tx.wait();
       const event = rc.events.find((event) => event.event === "GetRandomIndex");
       const random = BigNumber.from(event["topics"][2]);
