ebabb667fcdd6bcee260256652ac8ca3a7fd77cb
Jack Cheng <jackcpku@gmail.com>
2022-05-13 10:52:20 +0800

    Modify Deposit & Withdraw

--- contracts/nft/BasicERC20Bridge.sol
+++ contracts/nft/BasicERC20Bridge.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 interface ITransferable {
     function transferFrom(
         address sender,
@@ -9,12 +11,17 @@ interface ITransferable {
     ) external;
 }
 
-contract BasicERC20Bridge {
+contract BasicERC20Bridge is Ownable {
     event Deposit(
         address indexed depositor,
-        address depositAddress,
         uint256 indexed amount,
         uint256 indexed uid
+    );
+
+    event Withdraw(
+        address indexed to,
+        uint256 indexed amount,
+        uint256 indexed salt
     );
 
     function deposit(
@@ -28,6 +35,21 @@ contract BasicERC20Bridge {
             _amount
         );
 
-        emit Deposit(msg.sender, address(this), _amount, _uid);
+        emit Deposit(msg.sender, _amount, _uid);
+    }
+
+    function withdraw(
+        address _erc20TokenAddress,
+        address _to,
+        uint256 _amount,
+        uint256 _salt
+    ) external onlyOwner {
+        ITransferable(_erc20TokenAddress).transferFrom(
+            address(this),
+            _to,
+            _amount
+        );
+
+        emit Withdraw(_to, _amount, _salt);
     }
 }
